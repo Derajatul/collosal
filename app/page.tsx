bc1bb52eed48da7d998ab1a2b31c41fed764b459
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-23 03:18:04 +0000

    add how we work section

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
     <>
       <main className='flex items-center justify-center text-center flex-col'>
         {/* Hero section */}
-        <section className="min-h-screen flex justify-center items-center flex-col">
+        <section className="section flex justify-center items-center flex-col">
           <div className='max-w-[601px]'>
-            <h3 className="text-green text-base">CLIENT-DEVELOPMENT DRIVEN</h3>
-            <h1 className="text-white text-4xl font-bold leading-[54px]">We Design, We Develop. We Ship. In The Same Day.</h1>
-            <p className="text-muted font-medium leading-8">We are committed to not making clients wait. We will deliver the work as quickly as possible. Even on the same day. Even so, we do not reduce the quality of our work.</p>
+            <h3 className="green_heading">CLIENT-DEVELOPMENT DRIVEN</h3>
+            <h1 className="heading_1">We Design, We Develop. We Ship. In The Same Day.</h1>
+            <p className="paragraph">We are committed to not making clients wait. We will deliver the work as quickly as possible. Even on the same day. Even so, we do not reduce the quality of our work.</p>
           </div>
   
           <div className="mt-5 sm:flex sm:gap-5">
@@ -22,7 +22,7 @@ export default function Home() {
         </section>
   
         {/* Service section */}
-        <section className="min-h-screen w-full flex justify-center items-center flex-col">
+        <section className="section flex justify-center items-center flex-col">
           <div className="sm:flex sm:flex-wrap sm:gap-5 sm:items-center sm:justify-center">
             <FeatureCard icon="/featureCard/figma.svg" title="Design" description="The project interface will be designed first, our favorite tool is Figma." />
             <FeatureCard icon="/featureCard/code.svg" title="Develop" description="Transform design and write business logic here. Choose the technology you want." />
@@ -37,11 +37,24 @@ export default function Home() {
             <Image src='/company/microsoft.svg'  width={119} height={25} alt=''/>
             <Image src='/company/facebook.svg'  width={119} height={23} alt=''/>
           </div>
-          <div className="w-full h-px bg-[#FFFFFF1A] mt-5"></div>
+          <div className="w-full h-px bg-[#FFFFFF1A] mb-14 mt-5"></div>
           
         </section>
 
-        
+        {/* How we work section */}
+        <section className="section flex flex-wrap items-center justify-center">
+          <Image 
+            src="/howWeWork/illustration.svg" 
+            width={550} 
+            height={500} 
+            style={{ maxWidth: '100%', height: 'auto'}} 
+            alt=''/>
+          <div className="w-[502px] text-left">
+            <h3 className="green_heading">HOW WE WORK?</h3>
+            <h2 className="heading_2">Everything is well planned, well designed and developed wholeheartedly</h2>
+            <p className="paragraph">Careful planning makes us confident, developed with best practices so that the project can be maintained. We always test projects before they are shipped.</p>
+          </div>
+        </section>
       </main>
     </>
   )
