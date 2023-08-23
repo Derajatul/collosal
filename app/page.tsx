04a77f352fa009ca4b1b3603506c4f8061cb398f
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-23 03:17:41 +0000

    add how we work section ✅

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <main className='flex items-center justify-center text-center flex-col'>
         {/* Hero section */}
-        <section className="min-h-screen flex justify-center items-center flex-col my-10 sm:my-0">
+        <section className="min-h-screen flex justify-center items-center flex-col">
           <div className='max-w-[601px]'>
             <h3 className="text-green text-base">CLIENT-DEVELOPMENT DRIVEN</h3>
             <h1 className="text-white text-4xl font-bold leading-[54px]">We Design, We Develop. We Ship. In The Same Day.</h1>
@@ -22,12 +22,13 @@ export default function Home() {
         </section>
   
         {/* Service section */}
-        {/* <section className="min-h-screen w-full flex justify-center items-center flex-col">
-          <div>
+        <section className="min-h-screen w-full flex justify-center items-center flex-col">
+          <div className="sm:flex sm:flex-wrap sm:gap-5 sm:items-center sm:justify-center">
             <FeatureCard icon="/featureCard/figma.svg" title="Design" description="The project interface will be designed first, our favorite tool is Figma." />
             <FeatureCard icon="/featureCard/code.svg" title="Develop" description="Transform design and write business logic here. Choose the technology you want." />
             <FeatureCard icon="/featureCard/box.svg" title="Ship" description="After the work is complete, we will send the project and all its assets to you." />
           </div>
+          
           <div className="w-full h-px bg-[#FFFFFF1A] mt-14 mb-5"></div>
           <div className="flex gap-10 justify-center items-center flex-wrap">
             <Image src='/company/github.svg' width={119} height={33} alt='' />
@@ -38,7 +39,9 @@ export default function Home() {
           </div>
           <div className="w-full h-px bg-[#FFFFFF1A] mt-5"></div>
           
-        </section> */}
+        </section>
+
+        
       </main>
     </>
   )
