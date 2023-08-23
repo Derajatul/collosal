42661a5b4565ce6fb77cf4281ef49c6d0c9da0c0
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-23 03:18:55 +0000

    fix responsive 🐛

--- app/components/FeatureCard.tsx
+++ app/components/FeatureCard.tsx
@@ -8,7 +8,7 @@ type FeatureCard = {
 
 const FeatureCard = ({icon, title, description}: FeatureCard) => {
   return(
-    <div className="w-[328px] h-[276px] bg-[#FFFFFF0D] p-10 text-left rounded">
+    <div className="w-full my-5 sm:w-[328px] h-[276px] bg-[#FFFFFF0D] p-10 text-left rounded">
       <div className="h-[50px] w-[50px] bg-[#FFFFFF08] flex justify-center items-center">
         <Image width={24} height={24} src={icon} alt='' />
       </div>
