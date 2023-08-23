44dbfc08be832a8b75757ea4d9d618690b5ce545
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-23 09:58:59 +0000

    add project section ✅

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Button from './components/Button'
 import Image from 'next/image'
 import FeatureCard from './components/FeatureCard'
+import ProjectCard from './components/ProjectCard'
 import Eclipse from './components/Eclipse'
 
 export default function Home() {
@@ -16,8 +17,21 @@ export default function Home() {
           </div>
   
           <div className="mt-5 sm:flex sm:gap-5">
-            <Button onHref="https://example.com" colorBg="primary" styleBg="solid" size="normal" borderButton="square">Send Quote</Button>
-            <Button onHref="https://example.com" colorBg="white" styleBg="light" size="normal" borderButton="square">Learn More</Button>
+            <Button 
+              onHref="https://example.com" 
+              color="primary"
+              style="solid" 
+              size="normal" 
+              border="square">
+                Send Quote
+            </Button>
+            
+            <Button 
+              onHref="https://example.com" 
+              color="white" 
+              style="light" 
+              size="normal" 
+              border="square">Learn More</Button>
           </div>
         </section>
   
@@ -29,7 +43,7 @@ export default function Home() {
             <FeatureCard icon="/featureCard/box.svg" title="Ship" description="After the work is complete, we will send the project and all its assets to you." />
           </div>
           
-          <div className="w-full h-px bg-[#FFFFFF1A] mt-14 mb-5"></div>
+          <div className="line mt-14 mb-5"></div>
           <div className="flex gap-10 justify-center items-center flex-wrap">
             <Image src='/company/github.svg' width={119} height={33} alt='' />
             <Image src='/company/forbes.svg'  width={119} height={32} alt=''/>
@@ -37,7 +51,7 @@ export default function Home() {
             <Image src='/company/microsoft.svg'  width={119} height={25} alt=''/>
             <Image src='/company/facebook.svg'  width={119} height={23} alt=''/>
           </div>
-          <div className="w-full h-px bg-[#FFFFFF1A] mb-14 mt-5"></div>
+          <div className="line mb-14 mt-5"></div>
           
         </section>
 
@@ -63,7 +77,14 @@ export default function Home() {
             <h3 className="green_heading">OUR TEAM</h3>
             <h2 className="heading_2">We're a team of designers, engineers and analysts</h2>
             <p className="paragraph">Our team consists of many creative people. We are committed to maintaining quality work as well as speed. These creative people work together to create maximum work results.</p>
-            <Button onHref="https://example.com" colorBg="white" styleBg="light" size="normal" borderButton="square">See Our Teams</Button>
+            <Button 
+              onHref="https://example.com" 
+              color="white" 
+              style="light" 
+              size="normal" 
+              border="square"> 
+                See Our Teams
+            </Button>
           </div>
           <Image 
             src="/team/illustration.svg" 
@@ -72,6 +93,26 @@ export default function Home() {
             style={{ maxWidth: "100%", height: "auto" }}
             alt="" 
           />
+        </section>
+
+        {/* Projects section */}
+        <section className="section flex items-center justify-center flex-col">
+          <div className="line"></div>
+          <div className="w-full sm:w-[500px]">
+            <h3 className="green_heading">PROJECTS</h3>
+            <h2 className="heading_2">We have completed many amazing projects that you will not believe</h2>
+          </div>
+          <div className="w-full sm:flex sm:flex-wrap sm:gap-5 sm:items-center sm:justify-center">
+            <ProjectCard 
+            title="The Mobile App Landing Page" 
+            description="A landing page for mobile app" 
+            type="mobile" />
+            <ProjectCard 
+            title="The Desktop App Landing Page"  
+            description="A landing page for desktop app" 
+            type="desktop" />
+          </div>
+          
         </section>
       </main>
     </>
