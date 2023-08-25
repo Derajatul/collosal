efe68e8651447e22d2680b90e61f53f02371f113
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-25 11:11:45 +0000

    add iconlist component ✅

---
+++ app/components/IconList.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+type IconList ={
+  keyValue: string,
+  value: string,
+  img: string
+}
+
+const IconList = ({keyValue,value,img}: IconList):JSX.Element => {
+  return(
+    <div className="flex items-center justify-start flex-wrap gap-5">
+      <div className="w-[70px] h-[70px] bg-[#FFFFFF0D] flex items-center justify-center">
+        <Image src={img} width={30} height={30} alt='' />
+      </div>
+      <div className="text-left">
+        <p className="paragraph m-0">{keyValue}</p>
+        <p className="paragraph !font-bold !text-white m-0">{value}</p>
+      </div>
+    </div>
+  )
+}
+
+export default IconList
