b2a14c8269f955578b98e21da60b9d9bc7b2b79d
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-25 03:48:50 +0000

    add project page ✅

---
+++ app/projects/page.tsx
@@ -0,0 +1,27 @@
+import ProjectCard from '../components/ProjectCard'
+
+const Projects = () => {
+  return(
+    <main className='flex items-center justify-center text-center flex-col'>
+      <section className="min-h-[50vh] flex justify-between w-full items-center flex-wrap">
+        <div className="w-[507px] text-left">
+          <h3 className="green_heading">PROJECTS</h3>
+          <h2 className="heading_2">We have completed many amazing projects that you will not believe</h2>
+        </div>
+
+        <select id="size" className="inputText block">
+          <option className="bg-transparent text-black" value="technology">Technology</option>
+          <option className="bg-transparent text-black" value="technology">Technology</option>  
+        </select>
+      </section>
+      <div className="line"></div>
+      <section className="section mt-[90px] mb-[203px] gap-5 flex flex-wrap justify-center">
+        <ProjectCard title="The Mobile App Landing Page" description="A landing page for mobile app" type="mobile" />
+        <ProjectCard title="The Mobile App Landing Page" description="A landing page for desktop app" type="desktop" />
+        <ProjectCard title="The Mobile App Landing Page" description="A landing page for desktop app" type="desktop" />
+        <ProjectCard title="The Mobile App Landing Page" description="A landing page for mobile app" type="mobile" />
+      </section>
+    </main>
+  )
+}
+export default Projects
