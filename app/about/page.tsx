1da22407ca3c6bbe1fd2665e3469e93b42aa5414
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-27 03:54:19 +0000

    fix no-unescaped-entities error 🐛

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className="text-left w-full sm:w-[544px]">
           <h3 className="green_heading">ABOUT</h3>
           <h2 className="heading_2">We are creative, smart and hardworking people</h2>
-          <p className="paragraph">Several creative people gather in the same place - that's Collosal. We collaborate to produce the best results, loved by clients and comfortable for users. Here we maintain togetherness even though with different backgrounds, all the people here are already experts in their respective fields.</p>
+          <p className="paragraph">Several creative people gather in the same place - that&apos;s Collosal. We collaborate to produce the best results, loved by clients and comfortable for users. Here we maintain togetherness even though with different backgrounds, all the people here are already experts in their respective fields.</p>
         </div>
 
         <Image src='/about/header.svg' width={362} height={479} style={{ maxWidth: '100%', height: 'auto'}} alt="" />
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -13,7 +13,7 @@ const Work = () => {
       <section className="section flex justify-center items-center flex-row-reverse flex-wrap gap-10">
         <div className="w-full sm:w-[502px] text-left">
           <h3 className="green_heading">STEP 01</h3>
-          <h2 className="heading_2">Let's talk about your company's problems first</h2>
+          <h2 className="heading_2">Let&apos;s talk about your company&apos;s problems first</h2>
           <p className="paragraph">After submitting the quote form, we will review the data, then we will contact you. You can discuss with our team regarding your problem and find a solution to that problem. In this step, you will discuss what application to build.</p>
         </div>
         <Image src="/howWeWork/discuss.svg" width={411} height={408} style={{ maxWidth: '100%', height: 'auto'}} alt="" />
@@ -33,7 +33,7 @@ const Work = () => {
         <div className="w-full sm:w-[502px] text-left">
           <h3 className="green_heading">STEP 03</h3>
           <h2 className="heading_2">The project is complete and we ship all the project assets, and access to the server</h2>
-          <p className="paragraph">We will be responsible for delivering all the project assets to you, don't worry. These assets include, design files, source code, server access, and so on. In this step, everything is done and the contract ends.</p>
+          <p className="paragraph">We will be responsible for delivering all the project assets to you, don&apos;t worry. These assets include, design files, source code, server access, and so on. In this step, everything is done and the contract ends.</p>
         </div>
         <Image src="/howWeWork/project.svg" width={559} height={401} style={{ maxWidth: '100%', height: 'auto'}} alt="" />
       </section>
