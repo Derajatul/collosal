a07d756f286c333572a6180d9dade3ae07d9f13b
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-27 03:59:32 +0000

    fix type error 🐛

--- app/components/IconList.tsx
+++ app/components/IconList.tsx
@@ -1,18 +1,18 @@
 import Image from 'next/image'
 type IconList ={
-  keyValue: string,
+  keyValue?: string,
   value: string,
   img: string
 }
 
-const IconList = ({keyValue='',value,img}: IconList):JSX.Element => {
+const IconList = ({keyValue,value,img}: IconList):JSX.Element => {
   return(
     <div className="flex items-center justify-start flex-wrap gap-5">
       <div className="w-[70px] h-[70px] bg-[#FFFFFF0D] flex items-center justify-center">
         <Image src={img} width={30} height={30} alt='' />
       </div>
       <div className="text-left">
-        <p className="paragraph m-0">{keyValue}</p>
+        {keyValue && <p className="paragraph m-0">{keyValue}</p>}
         <p className="paragraph !font-bold !text-white m-0">{value}</p>
       </div>
     </div>
